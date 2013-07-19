b1620e63a2af1610e272725029f41b37c99b28d5
airixly <airixly@gmail.com>
2013-07-19 22:36:46 +0800

    initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+##Gitosis-new-project
+--------------------
+A shell script for gitosis to generate a new git project.
+
+##usage
+
+	
+
---
+++ tool/gitosis.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Program:
+#    This program is used to generate a new git project with gitosis
+#History:
+#2013/07/18 Rocky Lee     version-0.0.1
+
+if [ -n "$1" ]; then
+	echo ">>Register project ..."
+	echo "\n[group $1]\nwritable = $1" >> ../gitosis.conf
+	cat authority.conf >> ../gitosis.conf
+
+	echo "\n>>Sync to server ..."
+	cd ../
+	git add .
+	git commit -m "add project $1"
+	git push origin master
+
+	echo "\n>>Create git repository named $1 ..."
+	cd ../
+	mkdir "$1" && cd "$1"
+	cp ../.gitignore .gitignore
+	git init
+	git add .gitignore
+	git commit -m 'add ignore file'
+	git remote add origin yfgit@172.18.68.43:$1
+	git push origin master
+else
+	echo "Error: Please input a project name"
+fi
